986f253d49f8f1eeeaff69e4685859190bf167a1
sl1degod <sl1degod86@gmail.com>
2023-11-20 15:51:18 +0300

    13

--- controller/ReportController.js
+++ controller/ReportController.js
@@ -1,16 +1,24 @@
 const database = require('../db/database')
-
+const moment = require('moment')
 class ReportController {
 
     async getAllReports(req, res) {
         const report = await database.query(`select report.id as id, concat(users.firstname, ' ', LEFT(users.secondname, 1), '. ', LEFT(users.lastname, 1), '.') as FIO, typeofviolations.name as violations, objects.name as object, objects.latitude as latitude, objects.longitude as longitude, reportviolations.image as violations_image, report.date_report as date, report.time_report as time from users, report, typeofviolations, reportviolations, objects where report.user_id = users.id and typeofviolations.id = reportviolations.violations_id and reportviolations.id = report.rep_vio_id and objects.id = report.object_id order by report.id desc`)
-        res.json(report.rows)
+        const formattedReports = report.rows.map((row) => {
+            const formattedDate = moment(row.date).format('YYYY-MM-DD');
+            return {
+                ...row,
+                date: formattedDate
+            };
+        });
+        res.json(formattedReports);
     }
 
     async getReports(req, res) {
         const id = req.params.id
         const report = await database.query(`select concat(users.firstname, \' \', users.secondname, \' \', users.lastname) as FIO, typeofviolations.name, reportviolations.image, report.date_report as date, report.time_report as time from users, report, typeofviolations, reportviolations where report.id = $1 and report.user_id = users.id and typeofviolations.id = reportviolations.violations_id and reportviolations.id = report.rep_vio_id`, [id])
         res.json(report.rows[0])
+
     }
 
     async createReportsVio(req, res) {
@@ -32,8 +40,11 @@ class ReportController {
 
         try {
             const report = await database.query(`insert into report(user_id, rep_vio_id, object_id, date_report, time_report) values($1, $2, $3, current_date, current_time) RETURNING *`, [user_id, rep_vio_id, object_id])
-            res.json(report.rows[0])
-
+            // res.json(report.rows[0])
+            const responseData = report.rows[0];
+            const formattedDate = moment(responseData.date_report).format()
+            responseData.date_report = formattedDate;
+            res.json(responseData);
         } catch (error) {
             res.json({
                 message: "Во время создания произошла ошибка"
