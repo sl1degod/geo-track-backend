4a1c7b92bf2961046bae1cc6109d6fdeca4c6783
sl1degod <sl1degod86@gmail.com>
2023-11-22 20:30:12 +0300

    1331

--- controller/ViolationsController.js
+++ controller/ViolationsController.js
@@ -1,4 +1,5 @@
 const database = require('../db/database')
+const moment = require("moment/moment");
 
 class ViolationsController {
 
@@ -11,6 +12,23 @@ class ViolationsController {
         const id = req.params.id
         const vio = await database.query('select * from TypeOfViolations where id = $1', [id])
         res.json(vio.rows[0])
+    }
+
+    async getCharViolations(req, res) {
+        const vio = await database.query(`select typeofviolations.id, report.date_report as date, count (report.object_id) as count, typeofviolations.name as violation 
+                                            from report 
+                                            left join reportviolations on reportviolations.id = report.rep_vio_id 
+                                            left join typeofviolations on typeofviolations.id = reportviolations.violations_id 
+                                            group by date, report.object_id, violation, typeofviolations.id
+                                            order by violation`)
+        const formattedVio= vio.rows.map((row) => {
+            const formattedDate = moment(row.date).format('YYYY-MM-DD');
+            return {
+                ...row,
+                date: formattedDate
+            };
+        });
+        res.json(formattedVio);
     }
 }
 
--- routes/violations.js
+++ routes/violations.js
@@ -3,6 +3,7 @@ const violationsController = require('../controller/ViolationsController')
 const router = new Router();
 
 router.get('/violations', violationsController.getAllViolations);
+router.get('/violationsChar', violationsController.getCharViolations);
 router.get('/violations/:id', violationsController.getViolations);
 
 module.exports = router
