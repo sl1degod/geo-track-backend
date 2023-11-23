b82b4e5df5e571e0eaed28e5d983e0ffef324e02
sl1degod <sl1degod86@gmail.com>
2023-11-23 10:24:56 +0300

    1213

--- db/database.js
+++ db/database.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
     user:"postgres",
-    password: '123',
+    password: 'ilyas2701',
     host: "localhost",
     port: 5432,
     database: "kursovaya"
