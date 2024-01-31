39d081b030a00c477075418613a606fc2f57ee07
sl1degod <sl1degod86@gmail.com>
2024-01-31 22:02:17 +0300

    1234

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -1,5 +1,4 @@
 const database = require('../db/database')
-const bcrypt = require('bcrypt');
 const { generateAccessToken } = require('../config');
 
 class AuthController {
@@ -7,16 +6,17 @@ class AuthController {
         try {
             const {login, password} = req.body
             const user = await database.query(`select * from users where login = $1 and password = $2`, [login, password])
-            const validPassword = bcrypt.compareSync(password, user.rows[0].password)
+            if (!user.rows.length) {
+                return res.status(404).json({ message: "Пользователь не найден" });
+            }
             const token = generateAccessToken(user.rows[0].id, user.rows[0].login)
             return res.json({token})
-    
         } catch (e) {
           res.json({
               message: 'Что-то пошло не так'
           })
         }
-    }
+    } 
 }
 
 module.exports = new AuthController()
